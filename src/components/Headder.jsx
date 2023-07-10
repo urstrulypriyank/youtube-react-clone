4adadb263ede25d742475e0e7e00e162a3f872f9
urstrulypriyank <type2email@gmail.com>
2023-07-10 19:52:45 +0530

    added vitesvgr support

---
+++ src/components/Headder.jsx
@@ -0,0 +1,16 @@
+// import Logo from "../assets/youtubeLogo.svg";
+import Ham from "../assets/hamburger.png";
+import { ReactComponent as Logo } from "../assets/youtubeLogo.svg";
+const Headder = () => {
+  return (
+    <div className="w-[100vw] flex ">
+      {/* top Left */}
+      <Logo />
+      <section></section>
+      {/* SearchBar */}
+      {/* top Right*/}
+    </div>
+  );
+};
+
+export default Headder;
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import viteSvgr from "vite-plugin-svgr";
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [viteSvgr(), react()],
+});
