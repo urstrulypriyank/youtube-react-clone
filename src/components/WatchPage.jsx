42d144ddee3d322d406554c9f22618b29a7775d6
priyank rai <type2email@gmail.com>
2023-07-24 01:07:23 +0530

    emmbede yt video in /watchpage

--- src/components/WatchPage.jsx
+++ src/components/WatchPage.jsx
@@ -5,12 +5,23 @@ import { useSearchParams } from "react-router-dom";
 const WatchPage = () => {
   const dispatch = useDispatch();
   const [params] = useSearchParams();
-  console.log(params.get("v"));
+  const videoId = params.get("v");
   useEffect(() => {
     dispatch(hideMenu());
   }, []);
 
-  return <div>Namaste From watchpage</div>;
+  return (
+    <div>
+      <iframe
+        width="560"
+        height="315"
+        src={"https://www.youtube.com/embed/" + videoId}
+        title="YouTube video player"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        allowfullscreen
+      ></iframe>
+    </div>
+  );
 };
 
 export default WatchPage;
