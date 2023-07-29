3f89b300f4adb3089d587a1af528364990ef95c9
priyank rai <type2email@gmail.com>
2023-07-30 00:28:56 +0530

    improving

--- constant.js
+++ constant.js
@@ -2,7 +2,7 @@
 export const YT_MOST_POPULAR_API =
   " https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&regionCode=IN&maxResults=50&key=";
 export const YT_SEARCH_SUGGESTION_API =
-  "http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=";
+  "https://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=";
 
 export const YT_SEARCH_API =
   "https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25";
