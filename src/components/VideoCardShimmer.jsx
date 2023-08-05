c9522e301c3d1c504c833c597dc64844dd13d227
priyank rai <type2email@gmail.com>
2023-08-06 01:10:12 +0530

    fixing shimmer

--- src/components/VideoCardShimmer.jsx
+++ src/components/VideoCardShimmer.jsx
@@ -1,6 +1,7 @@
+import { useSelector } from "react-redux";
 const VideoCard = () => {
   return (
-    <div className="flex flex-col border shadow-lg w-[380px] h-40 rounded-lg my-4 mx-4 ">
+    <div className="flex flex-col border shadow-lg w-[300px] h-40 rounded-lg my-4 mx-4 ">
       <div className="relative h-[90%] w-full bg-gray-500 rounded-lg rounded-b-none "></div>
       <div className="mt-1">
         <p className="w-1/2 mb-2 h-3 animate-pulse bg-gray-500"></p>
@@ -12,9 +13,18 @@ const VideoCard = () => {
 };
 
 const VideoCardList = () => {
-  let arr = new Array(9).fill(0, 0, 9);
+  const substractWidht = useSelector(
+    (store) => store.menuState.sideBarWidthInRem
+  );
+  let arr = new Array(18).fill(0, 0, 19);
   return (
-    <div className="w-full h-full flex flex-1 flex-wrap justify-around">
+    <div
+      className="w-full h-full flex flex-1 flex-wrap justify-around"
+      style={{
+        width: `calc(100% - ${substractWidht})`,
+        marginLeft: `${substractWidht}`,
+      }}
+    >
       {arr.map((item, index) => (
         <div key={index}>
           <VideoCard />
